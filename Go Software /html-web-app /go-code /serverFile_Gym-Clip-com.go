e0aad74f8bad7a1f225f4c30cab8af13b3cd6a2a
kyle2501@gmail.com <kyle2501@gmail.com>
2024-06-28 18:31:29 -0700

    update serverFile_Gym-Clip-com.go

--- Go Software /html-web-app /go-code /serverFile_Gym-Clip-com.go
+++ Go Software /html-web-app /go-code /serverFile_Gym-Clip-com.go
@@ -10,7 +10,86 @@ import (
 )
 
 
+func # Gym-Clip.com ~
+
+package main
+
+
+// .
+  
+import (
+  "os"
+  "log"
+		
+  "text/template"
+  "net/http"
+		
+  "cloud.google.com/go"
+    
+    
+)
+
+
+
+type htmlPageData struct {
+    pageTitle string
+    pagePath string
+    pageList []pageNav
+    
+}
+
+
+type pageNav struct {
+    pageTitle string
+    pageLink string
+}
+
+
+
+
+
+
 func app_welcome_center_page() {
+    
+    
+}
+
+
+
+// . appHandler
+func appHandler(w http.ResponseWriter, r *http.Request) {
+
+    if r.URL.Path != "/app" {
+    	http.NotFound(w, r)
+    	return
+    }
+    
+// ,
+
+  pageTitle := "www.Gym-Clip.com/ - Website App"
+  pagePath := r.URL.Path
+  
+  pageType := ".."
+  
+  
+  pageData := htmlPageData {
+      pageTitle: pageTitle,
+      pagePath: pagePath,
+      
+      pageList: []pageNav {
+          { pageTitle: "one", pageLink: "one"},
+          { pageTitle: "two", pageLink: "two"},
+          { pageTitle: "three", pageLink: "three"},
+      },
+  	
+  }  //. .  pageData
+  
+  
+  pageFilePath := template.Must(template.ParseFiles("layout_main_page.html"))
+  pageFilePath.Execute(w, pageData)
+  
+}  //  .  appHandler
+() {
     
     
 }
