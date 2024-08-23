bcdb0618b8b239876c4dbe2892db3189d5547a35
kyle2501@gmail.com <kyle2501@gmail.com>
2024-08-23 16:34:38 -0700

    update serverFile_Gym-Clip-com.go

--- Go Software /html-web-app /go-code /serverFile_Gym-Clip-com.go
+++ Go Software /html-web-app /go-code /serverFile_Gym-Clip-com.go
@@ -1,23 +1,17 @@
-# Gym-Clip.com ~
+# app _ Gym-Clip.com ~
 
 package main
 
-
 // .
-  
 import (
   "os"
   "log"
 		
   "text/template"
   "net/http"
-		
   "cloud.google.com/go"
     
-    
 )
-
-
 
 type htmlPageData struct {
     pageTitle string
@@ -26,24 +20,20 @@ type htmlPageData struct {
     
 }
 
-
+// _ ` ~
 type pageNav struct {
     pageTitle string
     pageLink string
 }
 
 
-
-
-
-
+// _ ` ~
 func app_welcome_center_page() {
-    
     
 }
 
 
-
+// _ ` ~
 // . appHandler
 func appHandler(w http.ResponseWriter, r *http.Request) {
 
@@ -53,7 +43,6 @@ func appHandler(w http.ResponseWriter, r *http.Request) {
     }
     
 // ,
-
   pageTitle := "www.Gym-Clip.com/ - Website App"
   pagePath := r.URL.Path
   
@@ -72,39 +61,49 @@ func appHandler(w http.ResponseWriter, r *http.Request) {
   	
   }  //. .  pageData
   
-  
+  // _ ` ~
     if pagePath == "/clients" {
       pageTitle = "Clients Page"
       pageList = pageList
   }
   
+  
+  // _ ` ~
     if pagePath == "/workouts" {
       pageTitle = "Workouts Page"
       pageList = pageList
   }
   
+  
+  // _ ` ~
     if pagePath == "/videos" {
       pageTitle = "Videos Page"
       pageList = pageList
   }
   
+  
+  // _ ` ~
     if pagePath == "/gym-page" {
       pageTitle = "Gym Page"
       pageList = pageList
   }
   
   
-  
+  // _ ` ~
     if pagePath == "/splash" {
       pageTitle = "Splash Page"
       pageList = pageList
   }
   
+  
+  // _ ` ~
     if pagePath == "/discord" {
       pageTitle = "Discord App Page"
       pageList = pageList
   }
   
+  
+  // _ ` ~
     if pagePath == "/facebook" {
       pageTitle = "Facebook App Page"
       pageList = pageList
@@ -112,22 +111,25 @@ func appHandler(w http.ResponseWriter, r *http.Request) {
   
   
   
+  // _ ` ~
     if pagePath == "/flutter" {
       pageTitle = "Flutter Page"
       pageList = pageList
   }
   
+  // _ ` ~
     if pagePath == "/desktop" {
       pageTitle = "Desktop App Page"
       pageList = pageList
   }
   
+  // _ ` ~
     if pagePath == "/html-app" {
       pageTitle = "HTML-App Page"
       pageList = pageList
   }
   
-  
+  // _ ` ~
   pageFilePath := template.Must(template.ParseFiles("layout_main_page.html"))
   pageFilePath.Execute(w, pageData)
   
@@ -136,25 +138,27 @@ func appHandler(w http.ResponseWriter, r *http.Request) {
 
 //  .  html url routes 
 //  .  as well as terminal cli logs
-
 func main() {
 
   appName := "www.Gym-Clip.com/ - Website App"
 
-
+// _ ` ~
   http.HandleFunc("/", indexHandler)
   http.HandleFunc("/account", indexHandler)
   http.HandleFunc("/profile", indexHandler)
   
+  // _ ` ~
   http.HandleFunc("/clients", indexHandler)
   http.HandleFunc("/workouts", indexHandler)
   http.HandleFunc("/videos", indexHandler)
   http.HandleFunc("/gym-page", indexHandler)
   
+  // _ ` ~
   http.HandleFunc("/splash", indexHandler)
   http.HandleFunc("/discord", indexHandler)
   http.HandleFunc("/facebook", indexHandler)
   
+  // _ ` ~
   http.HandleFunc("/flutter", indexHandler)
   http.HandleFunc("/desktop", indexHandler)
   http.HandleFunc("/html-app", indexHandler)
@@ -179,6 +183,4 @@ func main() {
     log.Fatal("Error Starting the HTTP Server :", err)
     return
   }
-
-
 }
